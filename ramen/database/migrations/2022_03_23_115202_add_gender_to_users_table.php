fbffecbc3040354e242500467b60374527fc7706
HoaiNamvn <doanhoainam098@gmail.com>
2022-03-24 00:37:55 +0900

    migration-database

--- ramen/database/migrations/2022_03_23_115202_add_gender_to_users_table.php
+++ ramen/database/migrations/2022_03_23_115202_add_gender_to_users_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateProductTable extends Migration
+class AddGenderToUsersTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,9 +13,9 @@ class CreateProductTable extends Migration
      */
     public function up()
     {
-        Schema::create('product', function (Blueprint $table) {
-            $table->id();
-            $table->timestamps();
+        Schema::table('users', function (Blueprint $table) {
+            //
+            $table -> enum('gender', ['male', 'female']) -> nullable();
         });
     }
 
@@ -26,6 +26,9 @@ class CreateProductTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('product');
+        Schema::table('users', function (Blueprint $table) {
+            //
+            $table -> dropColumn('gender');
+        });
     }
 }
