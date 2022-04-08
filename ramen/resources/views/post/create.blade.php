e6d81fda23beedd7c04bab6e7be1c6907db3e699
HoaiNamvn <doanhoainam098@gmail.com>
2022-04-08 12:02:23 +0900

    pre insert command

--- ramen/resources/views/post/create.blade.php
+++ ramen/resources/views/post/create.blade.php
@@ -5,6 +5,32 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+
+    <script src="https://cdn.tiny.cloud/1/wpco4cfq5m8m1srfz1q2ksys8z17jeeipl9dwuf0dm1vxj93/tinymce/4/tinymce.min.js"
+        referrerpolicy="origin"></script>
+
+    <script>
+        tinymce.init({
+            selector: 'textarea',
+            height: 300,
+            plugins: [
+                'advlist autolink link image lists charmap print preview hr anchor pagebreak',
+                'searchreplace wordcount visualblocks visualchars code fullscreen insertdatetime media nonbreaking',
+                'table emoticons template paste help'
+            ],
+            toolbar: 'undo redo | styleselect | bold italic | alignleft aligncenter alignright alignjustify | ' +
+                'bullist numlist outdent indent | link image | print preview media fullscreen | ' +
+                'forecolor backcolor emoticons | help',
+            menu: {
+                favs: {
+                    title: 'My Favorites',
+                    items: 'code visualaid | searchreplace | emoticons'
+                }
+            },
+            menubar: 'favs file edit view insert format tools table help',
+            content_css: 'css/content.css'
+        });
+    </script>
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.4.1/dist/css/bootstrap.min.css"
         integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
     <title>Document</title>
@@ -13,7 +39,7 @@
 <body>
     <div class="container">
         <h1> Thêm bài viết </h1>
-        {{-- # lỗi chung trên đầu  --}}
+        {{-- # lỗi chung trên đầu --}}
         {{-- @if ($errors->any())
             <div class="alert alert-danger">
                 <ul>
@@ -23,7 +49,7 @@
                 </ul>
             </div>
         @endif --}}
-        {!! Form::open(['url' => 'post/store', 'method' => 'POST','files'=>true]) !!}
+        {!! Form::open(['url' => 'post/store', 'method' => 'POST', 'files' => true]) !!}
         <div class="form-group">
             {!! Form::text('title', '', ['class' => 'form-control', 'placeholder' => 'Tiêu đề']) !!}
             @error('title')
@@ -38,7 +64,7 @@
             @enderror
         </div>
         <div class="form-group">
-            {!! Form::file('file', ['class'=>'form-control-file']) !!}
+            {!! Form::file('file', ['class' => 'form-control-file']) !!}
         </div>
         <div class="form-group">
             {!! Form::submit('Thêm mới ', ['name' => 'sm-add', 'class' => 'btn btn-dark']) !!}
--- ramen/resources/views/post/index.blade.php
+++ ramen/resources/views/post/index.blade.php
@@ -5,9 +5,17 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <script src="https://cdn.tiny.cloud/1/wpco4cfq5m8m1srfz1q2ksys8z17jeeipl9dwuf0dm1vxj93/tinymce/4/tinymce.min.js" referrerpolicy="origin"></script>
+
+    <script>
+      tinymce.init({
+        selector: 'textarea'
+      });
+    </script>
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.4.1/dist/css/bootstrap.min.css"
         integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
     <title>Document</title>
+
 </head>
 
 <body>
