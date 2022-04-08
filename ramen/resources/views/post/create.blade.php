7e8f68f8a657130874f7dcdabc7bb6df9897def9
HoaiNamvn <doanhoainam098@gmail.com>
2022-04-08 12:11:10 +0900

    pre command

--- ramen/resources/views/post/create.blade.php
+++ ramen/resources/views/post/create.blade.php
@@ -8,7 +8,6 @@
 
     <script src="https://cdn.tiny.cloud/1/wpco4cfq5m8m1srfz1q2ksys8z17jeeipl9dwuf0dm1vxj93/tinymce/4/tinymce.min.js"
         referrerpolicy="origin"></script>
-<<<<<<< HEAD
 
     <script>
         tinymce.init({
@@ -31,45 +30,6 @@
             menubar: 'favs file edit view insert format tools table help',
             content_css: 'css/content.css'
         });
-=======
-    <script>
-        var editor_config = {
-            path_absolute: "http://localhost/ramen/",
-            selector: "textarea",
-            plugins: [
-                "advlist autolink lists link image charmap print preview hr anchor pagebreak",
-                "searchreplace wordcount visualblocks visualchars code fullscreen",
-                "insertdatetime media nonbreaking save table contextmenu directionality",
-                "emoticons template paste textcolor colorpicker textpattern"
-            ],
-            toolbar: "insertfile undo redo | styleselect | bold italic | alignleft aligncenter alignright alignjustify | bullist numlist outdent indent | link image media",
-            relative_urls: false,
-            file_browser_callback: function(field_name, url, type, win) {
-                var x = window.innerWidth || document.documentElement.clientWidth || document.getElementsByTagName(
-                    'body')[0].clientWidth;
-                var y = window.innerHeight || document.documentElement.clientHeight || document
-                    .getElementsByTagName('body')[0].clientHeight;
-
-                var cmsURL = editor_config.path_absolute + 'laravel-filemanager?field_name=' + field_name;
-                if (type == 'image') {
-                    cmsURL = cmsURL + "&type=Images";
-                } else {
-                    cmsURL = cmsURL + "&type=Files";
-                }
-
-                tinyMCE.activeEditor.windowManager.open({
-                    file: cmsURL,
-                    title: 'Filemanager',
-                    width: x * 0.8,
-                    height: y * 0.8,
-                    resizable: "yes",
-                    close_previous: "no"
-                });
-            }
-        };
-
-        tinymce.init(editor_config);
->>>>>>> 48dd1483e2da5b7080a48325b816f0b6ad1fce91
     </script>
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.4.1/dist/css/bootstrap.min.css"
         integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
