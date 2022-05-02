70b8125fe86f815c189f17afee50ea5284988901
HoaiNamvn <doanhoainam098@gmail.com>
2022-05-02 21:37:08 +0900

    HOME-PAGE を導入

---
+++ user_manager/app/Http/Controllers/HomeController.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+
+class HomeController extends Controller
+{
+    /**
+     * Create a new controller instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        $this->middleware('auth');
+    }
+
+    /**
+     * Show the application dashboard.
+     *
+     * @return \Illuminate\Contracts\Support\Renderable
+     */
+    public function index()
+    {
+        // get email
+        // return Auth::user()->email;
+        //get id fast
+        return Auth::id();
+        return route('register');
+        return view('home');
+    }
+}
